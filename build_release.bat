597c34c031ffc2a816dc5cdc0bed1be3e04c39f2
kderef <kianheitkamp@outlook.com>
2025-10-04 00:04:07 +0200

    add sdl submod

--- build_release.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-odin build main_release -out:bin/game_release.exe -strict-style -vet -no-bounds-check -o:speed -subsystem:windows
--- build_release.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-odin build main_release -out:bin/game_release.bin -strict-style -vet -no-bounds-check -o:speed
